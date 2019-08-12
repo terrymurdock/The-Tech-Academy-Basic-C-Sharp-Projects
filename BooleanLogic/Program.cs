49af4aa652b4c1a118ce909d02c8dddf9ed1d3b0
Terry <terrymurdock@hotmail.com>
2019-08-11 17:22:46 -0700

    Upload Exercise Pg 75 files

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // All true
+            Console.WriteLine(true && false); //false
+            Console.WriteLine(true && true);  //true
+            Console.WriteLine(false && false); //false
+
+            // At least one true
+            Console.WriteLine(true || true);  //true
+            Console.WriteLine(true || false);  //true
+            Console.WriteLine(false || false); //false
+
+            // Left equals right
+            Console.WriteLine(true == true);  //true
+            Console.WriteLine(true == false); //false
+            Console.WriteLine(false == false);  //true
+
+            // Left does not equal right
+            Console.WriteLine(true != true); //false
+            Console.WriteLine(true != false);  //true
+            Console.WriteLine(false != false); //false
+
+            // Onle one is true
+            Console.WriteLine(true ^ true); //false
+            Console.WriteLine(true ^ false);  //true
+            Console.WriteLine(false ^ false); //false
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CarInsuranceApproval/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Car Insurance Approval
+What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            bool duiYesNo = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int numTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool isApproved = ((age >= 15) && (duiYesNo = true) && (numTickets <= 3));
+            Console.WriteLine("You have been approved for our car insurance: " + isApproved);
+            Console.ReadLine();
+        }
+    }
+}
