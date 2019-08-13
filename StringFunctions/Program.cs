b02cbca613486419eebb811ac1894bbe651b9c03
Terry <terrymurdock@hotmail.com>
2019-08-12 21:27:14 -0700

    Add Exercise Pg 97 files

---
+++ StringFunctions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Terry";
+            //// Escape sequences
+            //string quote = "The man said, \"Hello, Terry\".\n Hello on a new line. \n \t Hello on a tab.";
+            //// Double back slash equals a single back slash
+            //string fileName = "C:\\Users\\Terry";
+
+            //// String functions
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //// Length of a variable
+            //int length = name.Length;
+
+            //// All letters uppercase or lowercase
+            //name = name.ToUpper();
+            //name = name.ToLower();
+
+            //Console.WriteLine(length);
+            //Console.ReadLine();
+
+            //Strings are immutable - meaning they are not changed, a new string is created
+            //string name = "Terry";
+            //name = "Joe";
+
+            //StringBuilder Class - dynamic object that can expand and collapse without the expensive memory overhead
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Terry");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings_Pg97/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_Pg97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CONCATENATE THREE STRINGS");
+            string begin = "The cow ";
+            string middle = "jumped over ";
+            string end = "the moon.";
+            Console.WriteLine(begin + middle + end +
+                              Environment.NewLine);
+            
+            Console.WriteLine("CONVERT STRING TO UPPERCASE");
+            string myName = "terry murdock";
+            myName = myName.ToUpper();
+            Console.WriteLine(myName +
+                              Environment.NewLine);
+
+            Console.WriteLine("STRINGBUILDER PARAGRAPH");
+            StringBuilder para = new StringBuilder();
+            para.Append("My name is Terry Murdock. ");
+            para.Append("I am enrolled at The Tech Academy. ");
+            para.Append("I am studying Software Development.");
+            Console.WriteLine(para);
+
+            Console.ReadLine();
+        }
+    }
+}
