ea7cbc6e958e88ddb8c229f579d24b763f9673b2
Terry <terrymurdock@hotmail.com>
2019-09-09 15:40:03 -0700

    Update to correct code

--- CarInsuranceApproval_Pg75/Program.cs
+++ CarInsuranceApproval_Pg75/Program.cs
@@ -14,13 +14,13 @@ namespace CarInsuranceApproval
 What is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Have you ever had a DUI? True or False");
+            Console.WriteLine("You have a DUI? True or False");
             bool duiYesNo = Convert.ToBoolean(Console.ReadLine());
 
             Console.WriteLine("How many speeding tickets do you have?");
             int numTickets = Convert.ToInt32(Console.ReadLine());
 
-            bool isApproved = ((age >= 15) && (duiYesNo = true) && (numTickets <= 3));
+            bool isApproved = (age > 15) && true && (numTickets <= 3);
             Console.WriteLine("You have been approved for our car insurance: " + isApproved);
             Console.ReadLine();
         }
