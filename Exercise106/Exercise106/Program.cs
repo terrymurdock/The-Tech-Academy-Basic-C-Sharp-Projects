c9555d610ae6ab9a8de1982189ae8d76fb9d0489
terrytown <terrytown206@gmail.com>
2019-09-12 15:15:09 -0700

    Add updated files

---
+++ Exercise106/Exercise106/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numList = { 24, 36, 75, 98, 53 };
+                Console.WriteLine("[{0}]", string.Join(", ", numList));
+                Console.WriteLine("Please pick a number to divide into each of the numbers above:");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Here are the results:");
+
+                for (int i = 0; i < numList.Length; i++)
+                {
+                    Console.WriteLine(numList[i] / userNum);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
