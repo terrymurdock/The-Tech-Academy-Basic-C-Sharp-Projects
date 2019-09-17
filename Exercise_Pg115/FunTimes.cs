2ccef0c02eb61e40e3a62f52a6b262dc0ea02307
terrytown <terrytown206@gmail.com>
2019-09-17 16:58:24 -0700

    Add exercise files

---
+++ Exercise_Pg115/FunTimes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Pg115
+{
+    class FunTimes
+    {
+        public int subtractingFun(int a, int b = 1)
+        {
+            int result = a - b;
+            return result;
+        }
+
+
+    }
+}
---
+++ Exercise_Pg115/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_Pg115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunTimes ft = new FunTimes();
+
+            Console.WriteLine("Please enter a number: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("(Optional) Please enter another number:");
+            string numTwo = Console.ReadLine();
+            if (numTwo == "")
+            {
+                int newOne = ft.subtractingFun(numOne);
+                Console.WriteLine("Not entering a second value results in: " + newOne);
+            }
+            else
+            {
+                int newValue = ft.subtractingFun(numOne, Convert.ToInt32(numTwo));
+                Console.WriteLine("The resulting value is: " + newValue);
+            }
+            Console.ReadLine();
+        }
+    }
+}
