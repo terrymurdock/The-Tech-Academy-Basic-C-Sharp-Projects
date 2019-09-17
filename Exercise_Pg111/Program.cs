8d9188043a00ad10a7d3cfa1a712b172e479992d
terrytown <terrytown206@gmail.com>
2019-09-17 13:09:52 -0700

    Add exercise files

---
+++ Exercise_Pg111/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Pg111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a whole number on which to perform math operations:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            ThreeMethods three = new ThreeMethods();
+
+            int add = three.AddNum(userNum);
+            double div = three.DivNum(userNum);
+            double multi = three.MultiNum(userNum);
+            Console.WriteLine("Method 1 add value is " + add + "\n" +
+                              "Method 2 divide value is " + div + "\n" +
+                              "Method 3 multiply value is " + multi);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise_Pg111/ThreeMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Pg111
+{
+    public class ThreeMethods
+    {
+        public int AddNum(int addResult)
+        {
+            int x = 10;
+            int result = x + addResult;
+            return result;
+        }
+
+        public double DivNum(double divResult)
+        {
+            int y = 33;
+            double result = y / divResult;
+            return result;
+        }
+
+        public double MultiNum(double multiResult)
+        {
+            double z = 15.5;
+            double result = z * multiResult;
+            return result;
+        }
+    }
+}
