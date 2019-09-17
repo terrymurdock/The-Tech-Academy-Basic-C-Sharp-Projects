17511ae54953d96e7fe1710ceb11811bb2f7fbf4
terrytown <terrytown206@gmail.com>
2019-09-17 14:57:19 -0700

    Add exercise files

---
+++ Exercise_Pg113/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Exercise_Pg113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod vm = new VoidMethod();
+
+            vm.VoidMeth(2, 7);
+            vm.VoidMeth(x: 5, y: 15);
+        }
+    }
+}
---
+++ Exercise_Pg113/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Pg113
+{
+    class VoidMethod
+    {
+        public void VoidMeth(int x, int y)
+        {
+            x = 5;
+            int result = 10 / x;
+            Console.WriteLine("The value of y is " + y);
+            Console.ReadLine();
+        }
+
+    }
+}
