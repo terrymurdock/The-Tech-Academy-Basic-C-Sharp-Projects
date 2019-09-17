ee9aafbfa3324dd4ecd1f99c6866d8f0e8748276
terrytown <terrytown206@gmail.com>
2019-09-17 15:55:31 -0700

    Add exercise files

---
+++ Exercise_Pg114/FunTimes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Pg114
+{
+    public class FunTimes
+    {
+        public int MathFun(int multiNum)
+        {
+            int x = 5;
+            int result = x * multiNum;
+            return result;
+        }
+
+        public double MathFun(double divNum)
+        {
+            int y = 6;
+            int result = Convert.ToInt32(y / divNum);
+            return result;
+        }
+
+        public int MathFun(string strNum)
+        {
+            int z = 9;
+            int result = z + Convert.ToInt32(strNum);
+            return result;
+        }
+    }
+}
---
+++ Exercise_Pg114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_Pg114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunTimes ft = new FunTimes();
+
+            int multi = ft.MathFun(12);
+            double div = ft.MathFun(2.4);
+            int add = ft.MathFun("8");
+            Console.WriteLine(multi);
+            Console.WriteLine(div);
+            Console.WriteLine(add);
+            Console.ReadLine();
+        }
+    }
+}
