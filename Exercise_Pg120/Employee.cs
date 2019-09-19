55ea370e9519af8246636b525aa11183059e160e
terrytown <terrytown206@gmail.com>
2019-09-19 13:45:51 -0700

    Add exercise pg 120 solution

---
+++ Exercise_Pg120/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Pg120
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Exercise_Pg120/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_Pg120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
