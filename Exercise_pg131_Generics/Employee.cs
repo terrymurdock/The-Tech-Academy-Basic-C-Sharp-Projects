1d735ee12cdfacd931d49b8944fb02727f0a1f42
Terry <terrymurdock@hotmail.com>
2019-10-17 00:29:33 -0700

    Add new and updated files

---
+++ Exercise_pg131_Generics/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_pg131_Generics
+{
+    public class Employee<T>
+    {
+        public List<T> things { get; set; }
+    }
+}
---
+++ Exercise_pg131_Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_pg131_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> thingsString = new Employee<string>();
+            Employee<int> thingsInt = new Employee<int>();
+
+            List<string> Things = new List<string>() { "desk", "computer", "chair" };
+            _ = new List<int>() { 2, 4, 6 };
+
+            for (int i = 0; i < Things.Count; i++)
+                Console.WriteLine(Things[i]);
+            Console.ReadLine();
+        }
+    }
+}
