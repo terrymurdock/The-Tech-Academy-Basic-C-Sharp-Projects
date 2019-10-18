96d5504698f66885e0f82e0ca8cba35e28865716
Terry <terrymurdock@hotmail.com>
2019-10-18 11:06:46 -0700

    Add new and updated files

---
+++ Exercise_Pg137_Structs/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Pg137_Structs
+{
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Exercise_Pg137_Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Pg137_Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 10.5m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
