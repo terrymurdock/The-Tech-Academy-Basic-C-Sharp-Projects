67be5ceff666ecf41815f9ce52464e1df761eaf6
Terry <terrymurdock@hotmail.com>
2019-10-21 17:46:09 -0700

    Add new files

---
+++ Exercise_Pg154_FileIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise_Pg154_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide an number.");
+            string number = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\terrytown\Logs\numberlog.txt", number);
+
+            Console.WriteLine("Your number is: " + number + ".");
+            Console.ReadLine();
+        }
+    }
+}
